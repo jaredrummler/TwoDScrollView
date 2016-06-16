96337f2bf2cc5872311ea4763ab70876068051f8
Jared Rummler <jared@jaredrummler.com>
2016-06-16 03:12:14 -0700

    move class to new package

--- library/src/main/java/com/jrummyapps/android/widget/TwoDScrollView.java
+++ library/src/main/java/com/jrummyapps/android/widget/TwoDScrollView.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2006 The Android Open Source Project
+ * Copyright (C) 2016 Jared Rummler <jared.rummler@gmail.com>
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -15,7 +15,7 @@
  *
  */
 
-package com.jaredrummler.android.widget;
+package com.jrummyapps.android.widget;
 
 import android.content.Context;
 import android.graphics.Rect;
